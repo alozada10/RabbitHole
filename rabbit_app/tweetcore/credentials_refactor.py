5a53a554d624f61bff312e5b1cb5897a1fa0958e
Alejandro Lozada <a.lozada10@uniandes.edu.co>
2021-08-01 15:54:39 -0500

    New credentials handle

--- rabbit_app/tweetcore/credentials_refactor.py
+++ rabbit_app/tweetcore/credentials_refactor.py
@@ -9,7 +9,5 @@ def return_credentials() -> dict:
         "database": credentials.POSTGRES_DATABASE,
         "consumer_key": credentials.api_key,
         "consumer_secret": credentials.api_key_secret,
-        "access_token": credentials.access_token,
-        "access_secret": credentials.access_secret
     }
     return config
