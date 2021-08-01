d7da68e2ea0e59d0c13bea3403ee7ead053b74b1
Alejandro Lozada <a.lozada10@uniandes.edu.co>
2021-08-01 15:56:15 -0500

    New credentials connect

--- rabbit_app/tweetcore/tasks/twitter_connect.py
+++ rabbit_app/tweetcore/tasks/twitter_connect.py
@@ -5,9 +5,6 @@ def api_connect(configuration: dict = None) -> tp.auth.OAuthHandler:
     auth = tp.OAuthHandler(consumer_key=configuration["consumer_key"],
                            consumer_secret=configuration["consumer_secret"])
 
-    auth.set_access_token(key=configuration["access_token"],
-                          secret=configuration["access_secret"])
-
     return auth
 
 
