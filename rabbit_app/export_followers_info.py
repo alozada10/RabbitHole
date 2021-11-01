d1d09a2d43e05894b9ce846416954befc11b3e26
alozada10 <a.lozada10@uniandes.edu.co>
2021-11-01 17:47:43 +0100

    bot detector

--- rabbit_app/export_followers_info.py
+++ rabbit_app/export_followers_info.py
@@ -2,29 +2,44 @@ import tweepy as tp
 import time
 from tweetcore.tasks import users_master
 from tweetcore.lib.postgres_target import download_data
+import pandas as pd
 import django
 
 django.setup()
 
 
 def export_follower_info(configuration: dict = None) -> int:
-    query_info = '''
+    df_trash = pd.read_csv('data/blocked_users.csv')
+    black_list = df_trash.trash_users.values.tolist()
+    if len(black_list) == 0:
+        black_list_statement = ''
+    elif len(black_list) > 0:
+        black_list = [str(i) for i in black_list]
+        black_list_statement = f'''
+        and tw_id not in {str(black_list).replace('[', '(').replace(']', ')')}
+        '''
+    else:
+        print('Something odd happened!')
+        raise
+
+    query_info = f'''
     select tw_id
     from tweetcore_twusers
-    where tw_screen = '-999'
+    where (tw_screen = '-999'
     or name = '-999'
-    or extract(year from tw_account_created_at) = 1999 
-    order by random ()
+    or extract(year from tw_account_created_at) = 1999)
+    {black_list_statement}
     limit 900
     '''
 
-    query_latency = '''
-        select count(0) as count
-        from tweetcore_twusers
-        where tw_screen = '-999'
-        or name = '-999'
-        or extract(year from tw_account_created_at) = 1999
-        '''
+    query_latency = f'''
+    select count(0) as count
+    from tweetcore_twusers
+    where (tw_screen = '-999'
+    or name = '-999'
+    or extract(year from tw_account_created_at) = 1999)
+    {black_list_statement}
+    '''
 
     df_followers = download_data.pandas_df_from_postgre_query(configuration=configuration,
                                                               query=query_info)
@@ -53,8 +68,12 @@ def export_follower_info(configuration: dict = None) -> int:
             except tp.TweepError as error:
                 if error.args[0][0]['code'] in [63, 50, 131]:
                     msg = error.args[0][0]['message']
-                    print(f'--- {follower_id} ---')
-                    print(f'--- {msg} ---')
+                    new_row = f'{str(follower_id)}, {msg}'
+                    with open('data/blocked_users.csv', 'a') as file:
+                        file.write('\n')
+                        file.write(new_row)
+                    # print(f'--- {follower_id} ---')
+                    # print(f'--- {msg} ---')
                     balance += 1
                     pass
                 else:
@@ -77,4 +96,4 @@ if __name__ == "__main__":
         if sentinel == 0:
             break
         else:
-            continue #change 2
+            continue  # change 2
--- rabbit_app/tweetcore/tasks/tweets_master.py
+++ rabbit_app/tweetcore/tasks/tweets_master.py
@@ -1,24 +1,71 @@
 import twitter_connect as tc
 import tweepy as tp
+import pandas as pd
+import numpy as np
 
 
 def get_tweets_from_id(configuration: dict = None,
                        tw_id: str = None,
                        include_rt: bool = False,
-                       num_tweets: int = 10) -> dict:
+                       num_tweets: int = 10,
+                       stage: str = None) -> dict:
+    user_tweets = {"tw_id": tw_id}
     tweets = {}
     api = tc.get_client(configuration=configuration)
+    counter = 1
     for i in tp.Cursor(api.user_timeline,
                        id=tw_id,
                        include_rts=include_rt,
                        tweet_mode='extended').items(num_tweets):
-        breakpoint()
-        hashtag_temp = [j["text"] for j in i.entities["hashtags"]]
-        temp = {"hashtag": hashtag_temp,
-                "text": i.full_text}
-        tweets[str(i.created_at)] = temp
+        temp = {"tweet_id": i.id_str,
+                "created_at": i.created_at}
+        if stage == 'detect_macro':
+            pass
+        elif stage == 'detect_micro':
+            temp["lang"] = i.lang
+            user_tweets["number_followers"] = i.user.followers_count
+            user_tweets["number_following"] = i.user.friends_count
+            temp["tweet"] = i.full_text
+        elif stage == 'analysis':
+            temp["lang"] = i.lang
+            user_tweets["number_followers"] = i.user.followers_count
+            user_tweets["number_following"] = i.user.friends_count
+            temp["tweet"] = i.full_text
+            temp["hashtags"] = [j["text"] for j in i.entities["hashtags"]]
+            temp["in_reply_to_tw_id"] = i.in_reply_to_user_id_str
+            temp["in_reply_to_tweet_id"] = i.in_reply_to_status_id_str
+            temp["mentions"] = [j["id_str"] for j in i.entities["user_mentions"]]
+            temp["rt_tw_id"] = None
+            temp["rt_tweet_id"] = None
+            temp["rt_quoted_tw_id"] = None
+            temp["rt_quoted_tweet_id"] = None
+            if i.is_quote_status:
+                temp["rt_quoted_tw_id"] = i.quoted_status.user.id_str
+                temp["rt_quoted_tweet_id"] = i.quoted_status.id_str
+            try:
+                if i.retweeted_status is not None:
+                    temp["rt_tw_id"] = i.retweeted_status.user.id_str
+                    temp["rt_tweet_id"] = i.retweeted_status.id_str
+                    temp["tweet"] = None
+            except AttributeError:
+                pass
+        else:
+            print('Must select stage: detect_macro, detect_micro or analysis')
+            raise
+        tweets[counter] = temp
+        counter += 1
+    user_tweets["tweets"] = tweets
+    breakpoint()
+    return user_tweets
 
-    return tweets
+
+def measure_activity(user_tweets: dict = None,
+                     stage: str = None):
+    report = {}
+    data_tweets = pd.DataFrame(user_tweets["tweets"]).T
+    freq_seconds = data_tweets["created_at"].diff(periods=-1)
+    freq_seconds = freq_seconds.apply(lambda x: x/np.timedelta64(1, 's')).fillna(0).astype('int64').values[:-1]
+
 
 
 from rabbit_app.tweetcore import credentials_refactor
@@ -26,7 +73,7 @@ from rabbit_app.tweetcore import credentials_refactor
 conf = credentials_refactor.return_credentials()
 
 r = get_tweets_from_id(configuration=conf,
-                   tw_id="1393667839584784390",
-                   include_rt=True,
-                   num_tweets=10)
-breakpoint()
+                       tw_id="1393667839584784390",
+                       include_rt=True,
+                       num_tweets=6,
+                       stage='analysis')
